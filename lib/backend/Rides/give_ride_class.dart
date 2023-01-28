e734f063211438d8b367ea62298024bffa96fa8c
Eric Shields <shieldse01@yahoo.com>
2023-01-28 16:38:53 -0600

    added change

---
+++ lib/backend/Rides/give_ride_class.dart
@@ -0,0 +1,11 @@
+class giveRide {
+  final String destination;
+  final int day;
+  final int month;
+  final int year;
+  final int leavingTime;
+  final int timeOutofWay;
+
+  giveRide(this.destination, this.day, this.month, this.year, this.leavingTime,
+      this.timeOutofWay);
+}
