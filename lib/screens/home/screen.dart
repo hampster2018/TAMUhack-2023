e1e956c339cb6e6f17a681cc6ec85d15eb871964
Shamitha Thumma <sthumma6104@gmail.com>
2023-01-28 14:10:59 -0600

    home is a stateful widget

--- lib/screens/home/screen.dart
+++ lib/screens/home/screen.dart
@@ -2,9 +2,14 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:tamuhack/globals/app_colors.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
