9a8a0a909fcd87302dc0dd1f7d8a7ade64afd633
Eric Shields <shieldse01@yahoo.com>
2023-01-28 15:09:33 -0600

    added add interest

--- lib/backend/User/add_interest.dart
+++ lib/backend/User/add_interest.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import '../../globals/user.dart' as user;
 
 // Import the firebase_core and cloud_firestore plugin
-import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class AddInterest extends StatelessWidget {
