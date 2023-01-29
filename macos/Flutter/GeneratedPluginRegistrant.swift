cdb78544ad7ab7366fd07dc98441c46103ba8df4
Eric Shields <shieldse01@yahoo.com>
2023-01-29 09:13:33 -0600

    random file

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -10,11 +10,8 @@ import firebase_auth
 import firebase_core
 import package_info_plus
 import firebase_storage
-<<<<<<< HEAD
-=======
 import geolocator_apple
 import location
->>>>>>> shamitha
 import path_provider_foundation
 import sqflite
 
@@ -24,11 +21,8 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FLTPackageInfoPlusPlugin"))
   FLTFirebaseStoragePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseStoragePlugin"))
-<<<<<<< HEAD
-=======
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   LocationPlugin.register(with: registry.registrar(forPlugin: "LocationPlugin"))
->>>>>>> shamitha
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
 }
